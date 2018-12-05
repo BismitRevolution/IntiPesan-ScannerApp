624f2c71519dac0d5d4770f8b4a0294b8c2a52e1
cymon1997 <cybermonster82@yahoo.com>
2018-12-05 19:00:42 +0700

    added ask permission for newer version

--- app/src/main/java/id/co/intipesan/intipesanscanner/MainActivity.java
+++ app/src/main/java/id/co/intipesan/intipesanscanner/MainActivity.java
@@ -1,7 +1,12 @@
 package id.co.intipesan.intipesanscanner;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -15,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private final AppCompatActivity activity = this;
     private CodeScannerView scannerView;
     private CodeScanner scanner;
+
+    private final int PERMISSION_REQUEST = 100;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,24 +43,53 @@ public class MainActivity extends AppCompatActivity {
         scannerView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                scanner.startPreview();
+                startScan();
             }
         });
+    }
+
+    private void startScan() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA }, PERMISSION_REQUEST);
+            }
+        } else {
+            scanner.startPreview();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    scanner.startPreview();
+                } else {
+                    errorPermissionDenied();
+                }
+                break;
+        }
+    }
+
+    private void errorPermissionDenied() {
+        Toast.makeText(getApplicationContext(), getResources().getString(R.string.error_permission_denied), Toast.LENGTH_SHORT).show();
+    }
+
+    private void activate(String data) {
+        Toast.makeText(getApplicationContext(), data, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        scanner.startPreview();
+        startScan();
     }
 
     @Override
     protected void onPause() {
         scanner.releaseResources();
         super.onPause();
-    }
-
-    private void activate(String data) {
-        Toast.makeText(getApplicationContext(), data, Toast.LENGTH_SHORT).show();
     }
 }
