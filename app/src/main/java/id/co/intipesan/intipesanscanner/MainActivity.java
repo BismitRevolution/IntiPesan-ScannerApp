f49bee5d54cfb10b27b291d147dcf36868796b28
cymon1997 <cybermonster82@yahoo.com>
2018-12-05 18:41:07 +0700

    added working camera scanner

--- app/src/main/java/id/co/intipesan/intipesanscanner/MainActivity.java
+++ app/src/main/java/id/co/intipesan/intipesanscanner/MainActivity.java
@@ -3,12 +3,57 @@ package id.co.intipesan.intipesanscanner;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 
 public class MainActivity extends AppCompatActivity {
+    private final AppCompatActivity activity = this;
+    private CodeScannerView scannerView;
+    private CodeScanner scanner;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scannerView = findViewById(R.id.scanner);
+        scanner = new CodeScanner(this, scannerView);
+        scanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(final Result result) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        activate(result.getText());
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanner.startPreview();
+            }
+        });
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        scanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        scanner.releaseResources();
+        super.onPause();
+    }
+
+    private void activate(String data) {
+        Toast.makeText(getApplicationContext(), data, Toast.LENGTH_SHORT).show();
     }
 }
